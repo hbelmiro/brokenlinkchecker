8896efb5997b38fc5947ed0e4fca091c5200ff79
hbelmiro <helber.belmiro@gmail.com>
2024-05-21 21:06:08 -0300

    Fixes
    Signed-off-by: hbelmiro <helber.belmiro@gmail.com>

--- src/main/java/com/hbelmiro/brokenlinkchecker/BrokenLinkChecker.java
+++ src/main/java/com/hbelmiro/brokenlinkchecker/BrokenLinkChecker.java
@@ -1,14 +1,19 @@
 package com.hbelmiro.brokenlinkchecker;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 @ApplicationScoped
 class BrokenLinkChecker {
 
+    @ConfigProperty(name = "com.hbelmiro.brokenlinkchecker.ignored-urls")
+    List<String> ignoredUrls;
+
     public void check(String rootContext, String page, VerificationOptions verificationOptions, Consumer<String> onCheckStatus) {
-        var verificationQueue = new VerificationQueue(rootContext, page, onCheckStatus);
+        var verificationQueue = new VerificationQueue(ignoredUrls, rootContext, page, onCheckStatus);
         verificationQueue.verify(verificationOptions);
     }
 }
--- src/main/java/com/hbelmiro/brokenlinkchecker/VerificationQueue.java
+++ src/main/java/com/hbelmiro/brokenlinkchecker/VerificationQueue.java
@@ -7,8 +7,10 @@ import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
 
 class VerificationQueue {
@@ -17,7 +19,7 @@ class VerificationQueue {
 
     private static final String NOT_OK = "⛔";
 
-    private final List<String> toVerify = new ArrayList<>();
+    private final Set<String> toVerify = new HashSet<>();
 
     private final Map<String, Verification> verifiedLinks = new HashMap<>();
 
@@ -27,7 +29,10 @@ class VerificationQueue {
 
     private final Consumer<String> onCheckStatus;
 
-    VerificationQueue(String rootContext, String page, Consumer<String> onCheckStatus) {
+    private final List<String> ignoredUrls;
+
+    VerificationQueue(List<String> ignoredUrls, String rootContext, String page, Consumer<String> onCheckStatus) {
+        this.ignoredUrls = ignoredUrls;
         this.rootContext = rootContext;
         this.page = page;
         this.onCheckStatus = onCheckStatus;
@@ -39,6 +44,9 @@ class VerificationQueue {
         try {
             document = Jsoup.connect(url).get();
         } catch (Exception e) {
+            Verification verification = new Verification();
+            verification.setStatus(NOT_OK);
+            verifiedLinks.put(url, verification);
             return List.of();
         }
 
@@ -48,7 +56,9 @@ class VerificationQueue {
 
         for (Element link : links) {
             String linkHref = link.attr("href");
-            if (linkHref.startsWith("/")) {
+            if (linkHref.startsWith("//")) {
+                linkHref = "http:" + linkHref;
+            } else if (linkHref.startsWith("/")) {
                 linkHref = rootContext + linkHref;
             } else if (linkHref.startsWith("#")) {
                 linkHref = url + linkHref;
@@ -56,10 +66,17 @@ class VerificationQueue {
             if (linkHref.endsWith("/")) {
                 linkHref = linkHref.substring(0, linkHref.length() - 1);
             }
-            linksToReturn.add(linkHref);
+
+            if (!isIgnored(linkHref)) {
+                linksToReturn.add(linkHref);
+            }
         }
 
         return linksToReturn;
+    }
+
+    private boolean isIgnored(String linkHref) {
+        return ignoredUrls.stream().anyMatch(linkHref::matches);
     }
 
     private String checkUrl(String url) {
@@ -69,23 +86,16 @@ class VerificationQueue {
         } else {
             try {
                 Jsoup.connect(url).get();
-                verification = new Verification();
-                verification.setStatus(OK);
-                verifiedLinks.put(url, verification);
                 return OK;
             } catch (Exception e) {
-                verification = new Verification();
-                String status = NOT_OK;
-                verification.setStatus(status);
-                verifiedLinks.put(url, verification);
-                return status;
+                return NOT_OK;
             }
         }
     }
 
     public void verify(VerificationOptions verificationOptions) {
         while (!toVerify.isEmpty()) {
-            String current = toVerify.get(0);
+            String current = toVerify.stream().findAny().orElseThrow();
             toVerify.remove(current);
 
             onCheckStatus.accept("Getting links from: " + current);
@@ -100,9 +110,13 @@ class VerificationQueue {
                 } else {
                     onCheckStatus.accept("├───" + status + "─── " + link);
                 }
-                if (OK.equals(status) && link.startsWith(page) && !link.contains("#")) {
+                if (!verifiedLinks.containsKey(link) && OK.equals(status) && link.startsWith(page) && !link.contains("#")) {
                     toVerify.add(link);
                 }
+
+                var verification = new Verification();
+                verification.setStatus(OK);
+                verifiedLinks.put(current, verification);
             });
         }
     }
